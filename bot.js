d926da5fbae65382467208741f04a0921a185605
HaZZe <32200147+HaZZeAdv@users.noreply.github.com>
2020-05-20 22:16:06 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,12 +2,19 @@ const { Client, RichEmbed } = require("discord.js");
 const { prefix, token } = require("./data/config.json");
 
 const client = new Client({disableEveryone: true});
-
+const wait = require('util').promisify(setTimeout);
+const invites = {};
 
 client.on("ready", () => {
   client.user.setActivity(`docs.invitemanager.co!`, { type: "PLAYING" });
   client.user.setStatus("online");
   console.log("Invites are ready...");
+  wait(1000);
+  client.guilds.forEach(g => {
+    g.fetchInvites().then(guildInvites => {
+      invites[g.id] = guildInvites;
+    });
+  });
 });
 
 client.on("message", async message => {
@@ -51,5 +58,22 @@ client.on("message", async message => {
   }
 });
 
+client.on('guildMemberAdd', member => {
+  // To compare, we need to load the current invite list.
+  member.guild.fetchInvites().then(guildInvites => {
+    // This is the *existing* invites for the guild.
+    const ei = invites[member.guild.id];
+    // Update the cached invites for the guild.
+    invites[member.guild.id] = guildInvites;
+    // Look through the invites, find the one for which the uses went up.
+    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
+    // This is just to simplify the message being sent below (inviter doesn't have a tag property)
+    const inviter = client.users.get(invite.inviter.id);
+    // Get the log channel (change to your liking)
+    const logChannel = member.guild.channels.find(channel => channel.name === "welcome");
+    // A real basic message with the information we need. 
+    logChannel.send(`${member.user.tag} joined using invite code ${invite.code} from ${inviter.tag}. Invite was used ${invite.uses} times since its creation.`);
+  });
+});
 //client.login(token);
 client.login(process.env.TOKEN)
